f2c35e9684f79d911d25c3a55473f9a4643a12fa
Innei <tukon479@gmail.com>
2026-05-12 20:11:13 +0800

    🔓 chore: expose vercel bench routes

--- src/libs/next/proxy/define-config.ts
+++ src/libs/next/proxy/define-config.ts
@@ -174,6 +174,7 @@ export function defineConfig() {
     '/api/workflows(.*)',
     '/api/agent(.*)',
     '/api/dev(.*)',
+    '/api/bench(.*)',
     '/webapi(.*)',
     '/trpc(.*)',
     // version
